dfda5599da86acc6055f71dd5255765b7a1d917f
LoeiFy <loeify@gmail.com>
2015-07-06 23:12:17 +0800

    add

---
+++ assets/modules/debug.js
@@ -0,0 +1,20 @@
+function(s) {
+
+    var i = 0;
+
+    $(s).on('click', function() {
+        i ++;
+
+        if (i == 5) {
+            i = 0;
+
+            var ip = prompt('ip', '');
+            if (!ip) return;
+
+            var e = document.createElement('script');
+            e.setAttribute('src', 'http://'+ ip +':8080/target/target-script-min.js')
+            document.getElementsByTagName('body')[0].appendChild(e)
+        }
+    })
+
+};
